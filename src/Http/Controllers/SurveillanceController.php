e2c2bd4b0697d2e4e27e123b21ab724bd00ca96e
raoxikui <raoxikui@qq.com>
2025-12-22 17:48:36 +0800

    优化

--- src/Http/Controllers/SurveillanceController.php
+++ src/Http/Controllers/SurveillanceController.php
@@ -17,62 +17,36 @@ class SurveillanceController extends AdminController
         $crud = $this->baseCRUD()
             ->filterTogglable(false)
             ->headerToolbar([
-                $this->createButton(true, 'full'),
+                $this->createButton('drawer', 300),
                 ...$this->baseHeaderToolBar()
             ])
+            ->autoGenerateFilter()
+            ->affixHeader()
+            ->columnsTogglable()
+            ->footable(['expand' => 'first'])
             ->autoFillHeight(true)
             ->columns([
-
-
                 amis()->TableColumn('id', 'ID')->sortable(),
-                amis()->TableColumn('title', '名称'),
+                amis()->TableColumn('title', '名称')->width(200),
                 amis()->TableColumn('agree', '协议（源）'),
-                amis()->TableColumn('remote', '输入流（源）'),
+                amis()->TableColumn('remote', '输入流（源）')->width(300),
                 amis()->TableColumn('name', '输出流名称'),
                 amis()->TableColumn('url', '输出流地址'),
                 amis()->TableColumn('fix', '输出流格式'),
                 amis()->TableColumn('sip', '分流内网IP'),
                 amis()->TableColumn('port', '分流公网端口'),
                 amis()->TableColumn('piont', '经纬度'),
-                amis()->TableColumn('sort', '排序[0-255]'),
-                amis()->TableColumn('state', '状态：0禁用，1启用'),
-                amis()->TableColumn('hot', '热点：1是， 0否'),
-                amis()->TableColumn('top', '置顶：1是，0否'),
-                amis()->TableColumn('online', '1在线，0离线'),
+                amis()->TableColumn('sort', '排序'),
+                amis()->TableColumn('state', '状态')->set('type', 'switch'),
+                amis()->TableColumn('hot', '热点')->set('type', 'switch'),
+                amis()->TableColumn('top', '置顶')->set('type', 'switch'),
+                amis()->TableColumn('online', '在线')->set('type', 'switch'),
                 amis()->TableColumn('created_at', admin_trans('admin.created_at'))->type('datetime')->sortable(),
                 amis()->TableColumn('updated_at', admin_trans('admin.updated_at'))->type('datetime')->sortable(),
-                $this->rowActions('dialog')
-
-
-
-
-//                amis()->TableColumn('id', 'ID')->set('fixed','left')->sortable(),
-//                amis()->TableColumn('title', '名称')->set('width',200),
-//                amis()->TableColumn('agree', '协议（源）'),
-////				amis()->TableColumn('uuid', '账号'),
-////				amis()->TableColumn('pass', '密码'),
-////				amis()->TableColumn('ip', 'IP'),
-////				amis()->TableColumn('iport', '端口')->sortable(),
-////				amis()->TableColumn('path', '路径'),
-////				amis()->TableColumn('channel', '通道')->sortable(),
-////				amis()->TableColumn('subtype', '子通道')->sortable(),
-//                amis()->TableColumn('remote', '推流完整路径（源）'),
-//                amis()->TableColumn('name', '流名称'),
-//                amis()->TableColumn('url', '播放地址')
-//                    ->set('width', 100)
-//                    ->set('height',100)
-//                    ->set('type','video'),
-////				amis()->TableColumn('sip', '分流内网IP'),
-////				amis()->TableColumn('port', '分流公网端口')->sortable(),
-//                amis()->TableColumn('piont', '经纬度'),
-//                amis()->TableColumn('status', '状态')->set('type','switch'),
-//                amis()->TableColumn('hot', '热点')->set('type','switch')->sortable(),
-//                amis()->TableColumn('top', '置顶')->set('type','switch')->sortable(),
-//                amis()->TableColumn('type', '类别')->set('type','switch'),
-//                amis()->TableColumn('stat', '在线')->set('type','switch')->sortable(),
-//                amis()->TableColumn('created_at', __('admin.created_at'))->set('type', 'datetime')->sortable(),
-//                amis()->TableColumn('updated_at', __('admin.updated_at'))->set('type', 'datetime')->sortable(),
-//                $this->rowActions(true, 'sm')->set('fixed','right')
+                $this->rowActions('drawer',300)
+                    ->set('align','center')
+                    ->set('fixed','right')
+                    ->set('width',150)
             ]);
 
         return $this->baseList($crud);
@@ -80,57 +54,29 @@ class SurveillanceController extends AdminController
 
     public function form($isEdit = false): Form
     {
-        return $this->baseForm()->columnCount()->body([
-
+        return $this->baseForm()->mode('normal')->body([
 
             amis()->TextControl('title', '名称'),
-            amis()->TextControl('agree', '协议（源）'),
             amis()->TextControl('remote', '输入流（源）'),
-            amis()->TextControl('name', '输出流名称'),
-            amis()->TextControl('url', '输出流地址'),
-            amis()->TextControl('fix', '输出流格式'),
+            amis()->GroupControl()->body([
+                amis()->Video()
+                    ->isLive()
+                    ->videoType('video/x-flv')
+                    ->src('http://cfss.cc/cdn/hy/11602075.flv')
+                    ->static()
+                    ->className('border-solid border-1 border-color-[var(--colors-brand-5)] rounded-xl shadow-lg overflow-hidden clear-none'),
+            ]),
+            amis()->SelectControl('fix', '输出流格式')
+                ->options([['label' => 'video/x-flv', 'value' => 'flv'], ['label' => 'application/x-mpegURL-hls', 'value' => 'm3u8']]),
             amis()->TextControl('sip', '分流内网IP'),
-            amis()->TextControl('port', '分流公网端口'),
+            amis()->NumberControl('port', '分流公网端口'),
             amis()->TextControl('piont', '经纬度'),
-            amis()->TextControl('sort', '排序[0-255]'),
-            amis()->TextControl('state', '状态：0禁用，1启用'),
-            amis()->TextControl('hot', '热点：1是， 0否'),
-            amis()->TextControl('top', '置顶：1是，0否'),
-            amis()->TextControl('online', '1在线，0离线'),
-
-
-//            amis()->TextControl('title', '名称'),
-//            amis()->TextControl('agree', '协议（源）'),
-////			amis()->TextControl('uuid', '账号'),
-////			amis()->TextControl('pass', '密码'),
-////			amis()->TextControl('ip', 'IP'),
-////			amis()->TextControl('iport', '端口'),
-////			amis()->TextControl('path', '路径'),
-////			amis()->TextControl('channel', '通道'),
-////			amis()->TextControl('subtype', '子通道'),
-//            amis()->ImageControl('remote', '推流完整路径（源）'),
-//            amis()->TextControl('name', '流名称'),
-//
-//
-//
-//            amis()->GroupControl()->body([
-//                amis()->Video()
-//                    ->isLive()
-//                    ->videoType('video/x-flv')
-//                    ->src('http://cfss.cc/cdn/hy/11602075.flv')
-//                    ->static()
-//                    ->className('border-solid border-2 border-blue-500 rounded-xl shadow-lg overflow-hidden clear-none'),
-//            ]),
-//
-//
-////			amis()->TextControl('sip', '分流内网IP'),
-////			amis()->TextControl('port', '分流公网端口'),
-//            amis()->TextControl('piont', '经纬度'),
-//            amis()->SwitchControl('status', '状态'),
-//            amis()->SwitchControl('hot', '热点'),
-//            amis()->SwitchControl('top', '置顶'),
-//            amis()->SwitchControl('type', '类别'),
-//            amis()->SwitchControl('stat', '在线'),
+            amis()->NumberControl('sort', '排序[0-255]'),
+            amis()->SwitchControl('state', '状态'),
+            amis()->SwitchControl('hot', '热点'),
+            amis()->SwitchControl('top', '置顶'),
+            amis()->ButtonGroupControl('online', '设备状态')
+                ->options([['label' => '在线', 'value' => '1'], ['label' => '离线', 'value' => '0']]),
         ]);
     }
 
