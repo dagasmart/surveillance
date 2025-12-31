c62799965293be59cf6a5aa3c9b4a77ca2caf74f
raoxikui <raoxikui@qq.com>
2025-12-31 17:51:15 +0800

    优化

--- src/Services/SurveillanceStreamService.php
+++ src/Services/SurveillanceStreamService.php
@@ -2,6 +2,7 @@
 
 namespace DagaSmart\Surveillance\Services;
 
+use DagaSmart\Organization\Services\EnterpriseService;
 use DagaSmart\Surveillance\Models\SurveillanceStream;
 
 /**
@@ -13,4 +14,40 @@ use DagaSmart\Surveillance\Models\SurveillanceStream;
 class SurveillanceStreamService extends AdminService
 {
     protected string $modelName = SurveillanceStream::class;
+
+
+    /**
+     * 机构单位列表
+     */
+    public function getEnterpriseAll(): array
+    {
+        return (new EnterpriseService)->query()
+            ->select(['id as value', 'enterprise_name as label', 'id'])
+            ->get()
+            ->toArray();
+    }
+
+    /**
+     * 递归选择项
+     * @return array
+     */
+    public function options(): array
+    {
+        $id = request()->id;
+        $enterprise_id = request()->enterprise_id;
+        $data = $this->query()->from('biz_facility as a')
+            ->join('biz_enterprise_facility as b','a.id','=','b.facility_id')
+            ->select(['a.id as value', 'a.facility_name as label', 'a.id', 'a.parent_id'])
+            ->when($enterprise_id, function($query) use ($enterprise_id) {
+                $query->where('b.enterprise_id', $enterprise_id);
+            })
+            ->when($id, function($query) use ($id) {
+                $query->where('b.facility_id', '<>', $id);
+            })
+            ->get()
+            ->toArray();
+        return array2tree($data);
+    }
+
+
 }
