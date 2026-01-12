2aaf15d5dade6c8de0f2abaa78e57ab5f70c00b5
raoxikui <raoxikui@qq.com>
2026-01-12 17:39:25 +0800

    优化

--- src/Http/Middleware/CheckPackageMiddleware.php
+++ src/Http/Middleware/CheckPackageMiddleware.php
@@ -11,7 +11,7 @@ class CheckPackageMiddleware
 
     public function handle(Request $request, Closure $next)
     {
-        if (admin_extension_enabled('dagasmart.organization')) {
+        if (!admin_extension_enabled('dagasmart.organization')) {
             return Admin::response()->fail('没有找到「<font color="#f40">基础安装包</font>」，请进行安装并启用');
         }
         return $next($request);
